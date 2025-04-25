28094fa1b33706b975756d46913a429bfd7ff7f0
lucianopulido <lucianopulido1996@gmail.com>
2025-04-25 16:51:00 -0300

    chore: initialize project structure

---
+++ src/main/java/com/inditex/apiprice/ApiPriceApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.apiprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/apiprice/ApiPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.apiprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
